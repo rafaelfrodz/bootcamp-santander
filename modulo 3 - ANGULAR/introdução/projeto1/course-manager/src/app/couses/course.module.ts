d2cf737201138bfd20b3fa21d3e9d7f4125c7e98
Rafael César <contato.frodz@hotmail.com>
2022-06-18 20:58:14 -0300

    aula 3/3 | introdução ao angular concluida!

---
+++ modulo 3 - ANGULAR/introdução/projeto1/course-manager/src/app/couses/course.module.ts
@@ -0,0 +1,30 @@
+import { AppPipeModule } from '../shared/components/pipe/app-pipe.module';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { CourseInfoComponent } from './course-info.component';
+import { CourseListComponent } from './course-list.component';
+import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { StarModule } from '../shared/components/star/star.module'
+
+@NgModule({
+  declarations: [
+    CourseListComponent,
+    CourseInfoComponent
+  ], imports: [
+    CommonModule,
+    FormsModule,
+    StarModule,
+    AppPipeModule,
+    RouterModule.forChild([
+      {
+        path: 'courses', component: CourseListComponent
+      },
+      {
+        path: 'courses/info/:id', component: CourseInfoComponent
+      }
+    ])
+  ]
+})
+
+export class CourseModule { }
--- modulo 3 - ANGULAR/introdução/projeto1/course-manager/src/app/couses/course.service.ts
+++ modulo 3 - ANGULAR/introdução/projeto1/course-manager/src/app/couses/course.service.ts
@@ -1,5 +1,7 @@
 import { Course } from "./course";
 import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Observable } from 'rxjs';
 
 
 @Injectable({
@@ -7,19 +9,28 @@ import { Injectable } from "@angular/core";
 })
 export class CourseService {
 
-  retrieveAll(): Course[] {
-    return COURSES
+  private coursesUrl: string = 'http://localhost:3100/api/courses'
+
+  constructor(private httpClient: HttpClient) { }
+
+  retrieveAll(): Observable<Course[]> {
+    return this.httpClient.get<Course[]>(this.coursesUrl)
   }
 
-  retrieveById(id: number): Course {
-    return COURSES.find((courseItereator: Course) => courseItereator.id === id)!
+  retrieveById(id: number): Observable<Course> {
+    return this.httpClient.get<Course>(`${this.coursesUrl}/${id}`)
   }
 
-  save(course: Course): void {
+  save(course: Course): Observable<Course> {
     if (course.id) {
-      const index = COURSES.findIndex((courseItereator: Course) => courseItereator.id === course.id)
-      COURSES[index] = course
+      return this.httpClient.put<Course>(`${this.coursesUrl}/${course.id}`, course)
+    } else {
+      return this.httpClient.post<Course>(`${this.coursesUrl}`, course)
     }
+  }
+
+  deleteById(id: number): Observable<any> {
+    return this.httpClient.delete<any>(`${this.coursesUrl}/${id}`)
   }
 
 }
